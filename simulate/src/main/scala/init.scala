9d3826586afe7db88b38d1aaa64e167337f62614
mattjp <mattjp@umich.edu>
2021-06-14 22:59:43 -0400

    read thru teams scala

--- simulate/src/main/scala/init.scala
+++ simulate/src/main/scala/init.scala
@@ -17,21 +17,33 @@ object Init {
 
 		val configJsonStr: String = scala.io.Source.fromFile("../resources/config.json").mkString
 		val secretsJsonStr: String = scala.io.Source.fromFile("../resources/secrets.json").mkString
+		val teamsJsonStr: String = scala.io.Source.fromFile("../resources/teams.json").mkString
 
 		val configJsonMap: Map[String, ujson.Value] = ujson.read(configJsonStr).obj.toMap
 		val secretsJsonMap: Map[String, ujson.Value] = ujson.read(secretsJsonStr).obj.toMap
+		val teamsJsonMap: Map[String, ujson.Value] = ujson.read(teamsJsonStr).obj.toMap
 
-		// val teams = 
+		// println(teamsJsonMap)
 
-		val rankingsUrl: String = Url(
-			scheme = "http", 
-			host = configJsonMap("base_url").str, 
-			path = configJsonMap("ratings").str,
-			query = QueryString.fromPairs("year" -> year, "team" -> "team")
-		).toString
+		val teams: Seq[String] = teamsJsonMap("teams")
+				.arr
+				.toSeq
+				.map(_.str)
+
+		// println(teams)
+		for (team <- teams) {
+			println(team) // first try
+		}
+
+		// val rankingsUrl: String = Url(
+		// 	scheme = "http", 
+		// 	host = configJsonMap("base_url").str, 
+		// 	path = configJsonMap("ratings").str,
+		// 	query = QueryString.fromPairs("year" -> year, "team" -> "team") // this doesn't work with different types - classic
+		// ).toString
 		
-		val apiKey: String = secretsJsonMap("api_key").str
-		val headers: Map[String, String] = Map("Authorization" -> apiKey)
+		// val apiKey: String = secretsJsonMap("api_key").str
+		// val headers: Map[String, String] = Map("Authorization" -> apiKey)
 
 		// val params = Map("year" -> 2000) // why do you not accept non-string params???
 		// val responseJson = ujson.read(requests.get(rankingsUrl, headers = headers).text)
